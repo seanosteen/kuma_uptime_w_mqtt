9ad41b7569ccd2d2a0623d1271b5862c1c53af5e
Sean O'Steen <sean@tinkeringrocks.com>
2024-08-27 02:05:42 +0000

    Restore DockerFile

--- build_multiarch.sh
+++ build_multiarch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker buildx build --no-cache\
-	--push --platform linux/amd64,linux/arm64,linux/arm/v7 \
+	--push --platform linux/amd64,linux/arm64 \
 	--tag seanosteen/kuma_uptime_w_mqtt:latest .
